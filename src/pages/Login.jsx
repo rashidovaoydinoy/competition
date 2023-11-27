e12216b0f2a10127b9e71fda6e8d99f1d2a6d939
Oydinoy <rashidova7777@bk.ru>
2023-11-27 16:48:46 +0500

    first commit

---
+++ src/pages/Login.jsx
@@ -0,0 +1,17 @@
+import Header from "../containers/Header";
+import Main from "../containers/Main";
+import Footer from "../containers/Footer";
+
+import "../containers/containers.scss";
+
+const Login = () => {
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  );
+};
+
+export default Login;
